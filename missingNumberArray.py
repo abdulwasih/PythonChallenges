f36291299ea14401798caccb2c1b3c4e17e90fcc
abdulwasih <vasih95@gmail.com>
2021-10-31 16:40:29 +0530

    completed

--- missingNumberArray.py
+++ missingNumberArray.py
@@ -1,8 +1,10 @@
 def missingnumber(inputlist):
-    listcount = len(inputlist)
+    listcount = len(inputlist) +1
     sumoflist = (listcount*(listcount+1))/2
     sumofinputlist = sum(inputlist)
     missingelement = sumoflist - sumofinputlist
     return missingelement
 
-    
+A = [1, 2, 4, 5, 6]
+print(missingnumber(A))
+# print(missingnumber(A))
